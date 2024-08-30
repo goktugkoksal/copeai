b06771070ccf93ca4f41b02b89cf441cee9e6eb6
Göktuğ Köksal <goktugkoksal@Goktugs-MacBook-Air.local>
2024-08-30 10:41:03 +0300

    0.0.1

---
+++ components/main/navbar.js
@@ -0,0 +1,61 @@
+export default function Navbar() {
+    return (
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <a class="logo" href="#">
+                    <img
+                        src="assets/imgs/copeai_long_logo.png"
+                        class="logo_img"
+                        alt="logo"
+                    />
+                </a>
+
+                <div
+                    class="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+                >
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">
+                                Home
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                Pricing
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                Works
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                Contact
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="topnav d-flex align-items-center">
+                    <a href="#" class="butn butn-rounded">
+                        <div class="d-flex align-items-center">
+                            <span>Request Demo</span>
+                            <span class="icon ml-10">
+                                <img
+                                    src="assets/imgs/icons/arrow-top-right.svg"
+                                    alt=""
+                                />
+                            </span>
+                        </div>
+                    </a>
+                    {/* <div class="menu-icon cursor-pointer">
+                        <span class="icon ti-align-right">
+                            <img src="" alt="" />
+                        </span>
+                    </div> */}
+                </div>
+            </div>
+        </nav>
+    );
+}
