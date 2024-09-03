fa03cb71c8f9d7c8ac6a5d53987d1e8101b5171f
Tunahan <114768777+tunahanbg@users.noreply.github.com>
2024-09-03 17:10:42 +0300

    first_commit

---
+++ components/main/footer.js
@@ -0,0 +1,92 @@
+export default function Footer() {
+    return (
+        <footer class="footer-sa pb-80">
+            <div class="container section-padding">
+                <div class="sec-head mb-80">
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <a href="#" class="logo md-mb80">
+                                <img src="assets/imgs/copeai_long_logo.png" alt="" />
+                            </a>
+                        </div>
+                        <div class="col-lg-6">
+                            <h3 class="text-indent">
+                                We hope to empower user and simplify their
+                                everyday lives.
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="contact-info">
+                    <div class="row">
+                        <div class="col-lg-6 offset-lg-5">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="item">
+                                        <span class="sub-color">location</span>
+                                        <p>
+                                            Teknokent, Reşitpaşa Mah. Katar Cad,
+                                            Arı Çk. No:3 Binası B109, 34485
+                                            Sarıyer/İstanbul
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 d-flex justify-content-end justify-end">
+                                    <div class="item">
+                                        <span class="sub-color">inquiry</span>
+                                        <p>info@copeai.com</p>
+                                        <p>(+90)542 504 2232</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {/* <div class="col-lg-6 offset-lg-5">
+                            <div class="social-icon">
+                                <a href="#0">
+                                    <i class="fa-brands fa-x-twitter"></i>
+                                </a>
+                                <a href="#0">
+                                    <i class="fa-brands fa-instagram"></i>
+                                </a>
+                                <a href="#0">
+                                    <i class="fa-brands fa-dribbble"></i>
+                                </a>
+                                <a href="#0">
+                                    <i class="fa-brands fa-behance"></i>
+                                </a>
+                                <a href="#0">
+                                    <i class="fa-brands fa-github"></i>
+                                </a>
+                            </div>
+                        </div> */}
+                    </div>
+                </div>
+            </div>
+            <div class="sub-footer">
+                <div class="container bord-thin-top-light pt-50">
+                    <div class="row">
+                        <div class="col-lg-8">
+                            <div class="copy sub-color md-mb50">
+                                <p>
+                                    © 2024
+                                    <a href="#0">CopeAI</a>. All Right
+                                    Reserved
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 d-flex justify-content-end">
+                            <div class="links sub-color d-flex justify-content-between">
+                                <a href="#" class="active">
+                                    Home
+                                </a>
+                                <a href="#">Pricing</a>
+                                <a href="#">Works</a>
+                                <a href="#">Contact</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,55 @@
-import "@/styles/globals.css";
+
+import "/public/css/plugins.css";
+import "/public/assets/css/home7-style.css"
+import "/styles/common_style.css"
+
+import Script from "next/script";
+import Layout  from "/states/layout";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+    return (
+        <Layout>
+            <Component {...pageProps} />
+            <Script
+                src="public/js/lib/jquery-3.6.0.min.js"
+                strategy="beforeInteractive"
+            ></Script>
+            <Script
+                src="public/js/lib/jquery-migrate-3.4.0.min.js"
+                strategy="beforeInteractive"
+            ></Script>
+
+            <Script
+                src="public/js/lib/plugins.js"
+                strategy="beforeInteractive"
+            ></Script>
+
+            <Script
+                src="public/js/gsap_lib/gsap.min.js"
+                strategy="beforeInteractive"
+            ></Script>
+            <Script
+                src="public/js/gsap_lib/ScrollSmoother.min.js"
+                strategy="beforeInteractive"
+            ></Script>
+            <Script
+                src="public/js/gsap_lib/ScrollTrigger.min.js"
+                strategy="beforeInteractive"
+            ></Script>
+            <Script
+                src="public/js/gsap_lib/SplitText.min.js"
+                strategy="beforeInteractive"
+            ></Script>
+
+            <Script
+                src="public/js/common_scripts.js"
+                strategy="beforeInteractive"
+            ></Script>
+
+            <Script
+                src="public/assets/js/scripts.js"
+                strategy="beforeInteractive"
+            ></Script>
+        </Layout>
+    );
 }
